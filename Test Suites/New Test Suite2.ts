058963c7a491f97987fcf6add51e381856f6f6b4
satyasai.samoju <satyasai.samoju@INBL01LAP31245.brd.g-c-i.com>
2023-07-25 14:37:04 +0530

    test suite 2 commit

---
+++ Test Suites/New Test Suite2.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite2</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>1a156469-fa7c-4ef1-a25c-292c551c6064</testSuiteGuid>
+   <testCaseLink>
+      <guid>0a6f842f-8c06-43a2-8907-c3b60d135e68</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/New Test Case 2</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
